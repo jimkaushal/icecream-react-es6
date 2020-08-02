538b979a04757b5492fb873a3a5b3b9cef9f3741
Jimmy Kaushal <jimkaushal@gmail.com>
2020-08-02 09:46:13 -0400

    now commit and push

--- src/components/tiles_list.component.js
+++ src/components/tiles_list.component.js
@@ -3,6 +3,7 @@ import Tile from './tile.component'
 
 const ListTiles = props => {
     const { stores, city } = props.location.state
+    console.log('YAHOO');
     return (
         <div className="container">
             <h1>{`${city}'s Top 12 Ice cream stores`}</h1>
